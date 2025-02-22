1daafb0780d1c42f55f11327ba9955648ad0bfb1
Chris233 <3475310230@qq.com>
2025-02-22 22:04:48 +0800

    maybe

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,20 @@ pub use clap::Parser;
 #[command(author = "H-Chris233")]
 #[command(version = "0.0.2")]
 pub struct ArgsIn{
-    ///the ip address or website you want to check [Default: bing.com]
+    ///the ip address or website you want to check
     //#[arg(short, long, default_values_t = vec!["bing.com".to_string(), "apple.com".to_string()])]
     #[arg(short, long, default_value = "bing.com")]
     pub ip: String,
     ///use -o to active when any connection losts
-    #[arg(short, long, default_value = "and")]
+    #[arg(short, long, default_value = "None")]
     pub or: String,
-    ///time between two normal check [Default: 60]
+    ///time between two normal check
     #[arg(short = 'n', long, default_value = "60")]
     pub secs_for_normal_loop: String,
-    ///time between two emegency check [Default: 20]
+    ///time between two emegency check
     #[arg(short = 'e', long, default_value = "20")]
     pub secs_for_emergency_loop: String,
-    ///times for emergency lopp [Default: 3]
+    ///times for emergency lopp
     #[arg(short, long, default_value = "3")]
     pub times_for_emergency_loop: String,
 }
