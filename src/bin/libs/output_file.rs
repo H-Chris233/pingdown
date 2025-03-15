9e860b4948cf2a39608d5285d257f969d1def7c4
H-Chris233 <3475310230@qq.com>
2025-03-15 08:50:35 +0800

    ...

--- src/bin/libs/output_file.rs
+++ src/bin/libs/output_file.rs
@@ -1,14 +1,13 @@
 use pingdown::RuntimeInfo;
 use std::fs::File;
 
-fn write_info(runtime_info: &RuntimeInfo) -> std::io::Result<()> {
-    let output_str = match std::fs::read_to_string("./pingdown_runtime_info.json") {
-        Ok(output_str) => output_str,
-        Err(_) => {
-            File::create("./pingdown_runtime_info.json")?;
-            "".to_string()
-        }
-    };
+pub fn write_info(runtime_info: &RuntimeInfo) -> Result<(), std::io::Error> {
+    let output_str = File::create("pingdown_runtime_info.txt")?;
+    
+    
+    
+    
+    Ok(())
 }
 
 
